7c843c7f84aef046c226e6a8196c46f874584c0d
thandal <than@timbrel.org>
2023-01-20 10:28:44 -0500

    clean up the script

--- parallel_process_radar_images.sh
+++ parallel_process_radar_images.sh
@@ -8,6 +8,4 @@ jupyter nbconvert --to python radar_images.ipynb
 
 ## 2. Run the .py in parallel:
 ## TODO: reduce memory usage to allow more parallel instantiations! (Can only run 4 with ~32 GB of RAM)
-#! ls -1 $DATA_PREFIX/*.img | xargs -n 1 basename | xargs -n 1 -P 4 python3 $script
-ls -1 $DATA_PREFIX/*1988*.img | xargs -n 1 basename | xargs -n 1 -P 4 python3 radar_images.py
-#! ls -1 *.img | xargs -n 1 -P 4 python3 ${cwd}/radar_images.py
+ls -1 $DATA_PREFIX/*.img | xargs -n 1 basename | xargs -n 1 -P 4 python3 radar_images.py
