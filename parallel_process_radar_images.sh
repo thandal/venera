04c5883971907ceb8959a4eb7188da0da5603717
thandal <than@timbrel.org>
2023-01-27 09:51:27 -0500

    rename to process_radar_images.

--- parallel_process_radar_images.sh
+++ parallel_process_radar_images.sh
@@ -4,9 +4,8 @@ ROOT_PREFIX="data_venus/arecibo_radar/pds-geosciences.wustl.edu/venus/arcb_nrao-
 DATA_PREFIX="$ROOT_PREFIX/data/"
 
 ## 1. Convert to notebook to .py
-jupyter nbconvert --to python radar_images.ipynb
+jupyter nbconvert --to python process_radar_images.ipynb
 
 ## 2. Run the .py in parallel:
 ## TODO: reduce memory usage to allow more parallel instantiations! (Can only run 4 with ~32 GB of RAM)
-ls -1 $DATA_PREFIX/*_$1*.img | xargs -n 1 basename | xargs -n 1 -P 4 python3 radar_images.py
-#ls -1 $DATA_PREFIX/*_$1*.img | xargs -n 1 basename | parallel --ungroup --retries 5 -n 1 -P 8 python3 radar_images.py {}
+ls -1 $DATA_PREFIX/*$1*.img | xargs -n 1 basename | xargs -n 1 -P 8 python3 process_radar_images.py
