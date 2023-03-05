6737db68e9196106c618e92d1af61aa9da030399
thandal <than@timbrel.org>
2023-03-05 07:59:58 -0500

    New figures and sobel method

--- rotation_period_experiment.py
+++ rotation_period_experiment.py
@@ -14,7 +14,7 @@ filenames = (
 #R = (-243.0185, )   # Official IAU value
 
 #R = np.unique(np.concatenate((
-#    -243.020 + np.linspace(-0.002, 0.002, 81),
+#    -243.018 + np.linspace(-0.002, 0.002, 81),
 #    )))
 #RA = (272.76, )  # Official IAU value
 #DEC = (67.16, )  # Official IAU value
@@ -32,26 +32,27 @@ print(f"{RA=}")
 print(f"{DEC=}")
 print(len(R) * len(RA) * len(DEC))
 
-NUM_SUBPROCESSES = 4
+NUM_SUBPROCESSES = 6
 SUBPROCESSES = []
 
-for r in R:
-    for ra in RA:
-        for dec in DEC:
-            print(r, ra, dec)
-            if len(SUBPROCESSES) >= NUM_SUBPROCESSES:
-                # Wait until the earliest subprocess is done
-                SUBPROCESSES.pop(0).communicate()
-            # Kludge to manipulate the poliastro library
-            for filename in filenames:
-                content = open(filename, "r").read()
-                content = content.replace("VENERA_RA", str(ra))
-                content = content.replace("VENERA_DEC", str(dec))
-                content = content.replace("VENERA_ROT_PER", str(r))
-                new_filename = filename.replace("TEMPLATE", "py")
-                open(new_filename, "w").write(content)
-            # This code automatically skips images that have already been processed.
-            s = subprocess.Popen("python3 process_radar_images.py", shell=True)
-            SUBPROCESSES.append(s)
-            # Sleep a little to allow the subprocess to successfully import poliastro
-            time.sleep(2)  
+if 1:
+    for r in R:
+        for ra in RA:
+            for dec in DEC:
+                print(r, ra, dec)
+                if len(SUBPROCESSES) >= NUM_SUBPROCESSES:
+                    # Wait until the earliest subprocess is done
+                    SUBPROCESSES.pop(0).communicate()
+                # Kludge to manipulate the poliastro library
+                for filename in filenames:
+                    content = open(filename, "r").read()
+                    content = content.replace("VENERA_RA", str(ra))
+                    content = content.replace("VENERA_DEC", str(dec))
+                    content = content.replace("VENERA_ROT_PER", str(r))
+                    new_filename = filename.replace("TEMPLATE", "py")
+                    open(new_filename, "w").write(content)
+                # This code automatically skips images that have already been processed.
+                s = subprocess.Popen("python3 process_radar_images.py", shell=True)
+                SUBPROCESSES.append(s)
+                # Sleep a little to allow the subprocess to successfully import poliastro
+                time.sleep(2)  
